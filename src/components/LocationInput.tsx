4b2091a3bafc47496dc450ffc5890766969c0de2
Anas Yaseen <a.yaseen@pythys.com>
2024-10-13 16:16:26 +0300

    Created forms with all their components for both searching and posting new jobs and added validations to them

---
+++ src/components/LocationInput.tsx
@@ -0,0 +1,62 @@
+import { forwardRef, useMemo, useState } from "react";
+import { Input } from "./ui/input";
+import citiesList from "@/lib/cities-list";
+
+interface LocationInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  onLocationSelected: (location: string) => void;
+}
+
+export default forwardRef<HTMLInputElement, LocationInputProps>(
+  function LocationInput({ onLocationSelected, ...props }, ref) {
+    const [locationSearchInput, setLocationSearchInput] = useState("");
+    const [isFocused, setIsFocused] = useState(false);
+
+    const cities = useMemo(() => {
+      if (!locationSearchInput.trim()) return [];
+      const searchWords = locationSearchInput.split("");
+      return citiesList
+        .map((city) => `${city.name}, ${city.subcountry}, ${city.country}`)
+        .filter(
+          (city) =>
+            city.toLowerCase().startsWith(searchWords[0].toLowerCase()) &&
+            searchWords.every((word) =>
+              city.toLowerCase().includes(word.toLowerCase()),
+            ),
+        )
+        .slice(0, 5);
+    }, [locationSearchInput]);
+    return (
+      <div className="relative">
+        <Input
+          placeholder="Search for a city"
+          type="search"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          value={locationSearchInput}
+          onChange={(e) => setLocationSearchInput(e.target.value)}
+          {...props}
+          ref={ref}
+        />
+        {locationSearchInput.trim() && isFocused && (
+          <div className="absolute z-20 w-full divide-y rounded-b-lg border-x border-b bg-background shadow-xl">
+            {!cities.length && <p className="p-3">No results found</p>}
+            {cities.map((city) => (
+              <button
+                className="block w-full p-2 text-start"
+                key={city}
+                onMouseDown={(e) => {
+                  e.preventDefault();
+                  onLocationSelected(city);
+                  setLocationSearchInput("");
+                }}
+              >
+                {city}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,64 @@
 import { z } from "zod";
+import { jobTypes, locationTypes } from "./job-types";
+
+const requiredString = z.string().min(1, "Required");
+const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
+
+const companyLogoSchema = z
+  .custom<File | undefined>()
+  .refine(
+    (file) => !file || (file instanceof File && file.type.startsWith("image/")),
+    "File is not an image",
+  )
+  .refine(
+    (file) => !file || file.size < 1024 * 1024 * 2, // A max size of 2MB,
+    "File must be less than 2MB",
+  );
+
+const applicationSchema = z
+  .object({
+    applicationEmail: z.string().max(100).email().optional().or(z.literal("")),
+    applicationUrl: z.string().max(100).email().optional().or(z.literal("")),
+  })
+  .refine((data) => data.applicationUrl || data.applicationEmail, {
+    message: "Email or url is required",
+    path: ["applicationEmail"],
+  });
+
+const locationSchema = z
+  .object({
+    locationType: requiredString.refine(
+      (value) => locationTypes.includes(value),
+      "Invalid location type",
+    ),
+    location: z.string().max(100).optional(),
+  })
+  .refine(
+    (data) =>
+      !data.locationType || data.locationType === "Remote" || data.location,
+    {
+      message: "Location is required for on-site jobs",
+      path: ["location"],
+    },
+  );
+
+export const createJobSchema = z
+  .object({
+    title: requiredString.max(100),
+    type: requiredString.refine(
+      (value) => jobTypes.includes(value),
+      "Invalid job type",
+    ),
+    companyName: requiredString.max(100),
+    companyLogo: companyLogoSchema,
+    description: z.string().max(5000).optional(),
+    salary: numericRequiredString.max(
+      9,
+      "Number can't be longer than 9 digits",
+    ),
+  })
+  .and(applicationSchema)
+  .and(locationSchema);
 
 export const jobFilterSchema = z.object({
   q: z.string().optional(),
@@ -8,3 +68,4 @@ export const jobFilterSchema = z.object({
 });
 
 export type JobFilterValues = z.infer<typeof jobFilterSchema>;
+export type CreateJobValues = z.infer<typeof createJobSchema>;
